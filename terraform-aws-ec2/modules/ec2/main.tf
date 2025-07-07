3f6506bd434b58c7f4b9d6e17a0c7708c6b59518
LuongTanTai200803 <107525476+LuongTanTai200803@users.noreply.github.com>
2025-07-07 21:35:43 +0700

    Update main.tf

--- terraform-aws-ec2/modules/ec2/main.tf
+++ terraform-aws-ec2/modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
   tags = {
     Name = "TerraformEC2"
   }
