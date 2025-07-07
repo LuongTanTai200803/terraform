da1a7a24ab636557bf6db7d3d9a9cadd0ac4f062
LuongTanTai200803 <107525476+LuongTanTai200803@users.noreply.github.com>
2025-07-07 21:28:27 +0700

    Update main.tf

--- terraform-aws-ec2/modules/ec2/main.tf
+++ terraform-aws-ec2/modules/ec2/main.tf
@@ -7,3 +7,47 @@ resource "aws_instance" "this" {
     Name = "TerraformEC2"
   }
 }
+
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring_sg"
+  description = "Allow SSH, Grafana, Loki, Promtail"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Loki"
+    from_port   = 3100
+    to_port     = 3100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Promtail push"
+    from_port   = 9080
+    to_port     = 9080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
